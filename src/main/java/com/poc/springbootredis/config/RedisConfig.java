17dd4aa255ad6fd233401810cd419c3d301a04a4
Artur Drummond <arturbdr@gmail.com>
2019-09-22 22:19:25 +0200

    - add: change redis driver to work with redisson

--- src/main/java/com/poc/springbootredis/config/RedisConfig.java
+++ src/main/java/com/poc/springbootredis/config/RedisConfig.java
@@ -1,17 +1,18 @@
 package com.poc.springbootredis.config;
 
+import org.redisson.api.RedissonClient;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 @Configuration
-public class RedisTemplateConfig {
+public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+    public RedisTemplate<String, String> redisTemplate(RedissonConnectionFactory factory) {
         final Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
 
         final StringRedisTemplate template = new StringRedisTemplate(factory);
@@ -19,5 +20,10 @@ public class RedisTemplateConfig {
         template.setKeySerializer(jackson2JsonRedisSerializer);
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean
+    RedissonConnectionFactory redissonConnectionFactory(final RedissonClient redisson) {
+        return new RedissonConnectionFactory(redisson);
     }
 }
